cf0815aa69da6352f20e5ae75f5b56c79b7039ac
truong120201 <truong120201@gmail.com>
2022-05-31 00:42:55 +0700

    git commit

--- pages/Home1/Home1.js
+++ pages/Home1/Home1.js
@@ -7,8 +7,24 @@ import { Header, TitleAndDesc, Contact, ProfessionalExpert } from '../../compone
 import { AboutUs, ListService, Slider } from '../../views/Home1/index.js'
 
 // MUI
-import useStyles from './styles.js'
 import { Box } from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core'
+
+import Home1SliderBackgr from '../../assets/images/Home1-slider-background.svg'
+import BgrBubbleRight from '../../assets/images/bgr-bubble-right.svg'
+import BgrBubbleLeft from '../../assets/images/bgr-bubble-left.svg'
+
+
+const useStyles = makeStyles({
+    home1: {
+        width: '100%',
+        backgroundImage: `url('${Home1SliderBackgr.src}'), url('${BgrBubbleRight.src}'), url('${BgrBubbleLeft.src}')`,
+        backgroundRepeat: 'no-repeat, no-repeat, no-repeat',
+        backgroundSize: '87% auto, auto, auto',
+        backgroundPosition: 'top left, top 1320px right 0, top 2835px left 0'
+    }
+})
 
 function Home1() {
     const classes = useStyles()
